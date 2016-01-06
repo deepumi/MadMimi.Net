d6446f2a7bd4ae87ee0ea52045ef238711567543
Deepu Madhusoodanan <deepumi@gmail.com>
2016-01-06 18:59:49 -0500

    adding files

---
+++ MadMimi.Net.Web/MadMimi.Net.Web/MadMimi.Net.Web/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+using MadMimi.Net;
+using System.Configuration;
+using System.Net.Mail;
+
+namespace MadMimi.Net.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public async Task<ActionResult> Index()
+        {
+
+            try
+            {
+                Mailer mailer = new Mailer(ConfigurationManager.AppSettings["MadMimiUserName"], ConfigurationManager.AppSettings["MadMimiApiKey"]);
+                using (var message = new MailMessage())
+                {
+                    message.From = new MailAddress("from email", "display name");
+                    message.To.Add("sender email");
+                    message.Subject = "Test";
+                    message.Body = "<h1>This is test</h1>";
+                    await mailer.SendAsync("test", message);
+                }
+                var status = mailer.TrackStatus;
+                var mailTransactionId = mailer.TransactionId;
+            }
+            catch (Exception)
+            {
+                 //log exception here
+            }
+            return View();
+        }
+    }
+}
---
+++ MadMimi.Net/MadMimi.Net/Mailer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace MadMimi.Net
+{
+    public class Mailer
+    {
+        private readonly string _userName;
+
+        private readonly string _apiKey;
+
+        public string TransactionId { get; private set; }
+
+        public string PromotionName { get; set; }
+
+        public string TrackStatus
+        {
+            get
+            {
+                return String.Format("https://madmimi.com/mailers/status/{0}?username={1}&api_key={2}", TransactionId, _userName, _apiKey);
+            }
+        }
+
+        public Mailer(string userName, string apiKey)
+        {
+            _userName = userName;
+            _apiKey = apiKey;
+        }
+
+        public async Task Send(MailMessage message)
+        {
+            await SendAsync(PromotionName, message);
+        }
+
+        public async Task SendAsync(string promotionName, MailMessage message, string url = "https://api.madmimi.com/mailer")
+        {
+            if (_userName == null) throw new ArgumentNullException("userName");
+            if (_apiKey == null) throw new ArgumentNullException("apiKey");
+            if (promotionName == null && PromotionName == null) throw new ArgumentNullException("promotionName");
+            if (message == null || message.To == null || message.From == null || message.Subject == null || message.Body == null) throw new ArgumentNullException("message");
+
+            var param = CreateParam(promotionName, message);
+            using (var client = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(HttpMethod.Post, url))
+                {
+                    request.Headers.Add("User-Agent", "MadMimiMailer.Net");
+                    request.Content = new FormUrlEncodedContent(param);
+                    using (var response = await client.SendAsync(request).ConfigureAwait(false))
+                    {
+                        var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                        if (response.StatusCode != System.Net.HttpStatusCode.OK) throw new Exception(result);
+                        TransactionId = result;
+                    }
+                }
+            }
+        }
+
+        private Dictionary<string, string> CreateParam(string promotionName, MailMessage message)
+        {
+            var param = new Dictionary<string, string>
+            {
+                {"username", _userName},
+                {"api_key", _apiKey},
+                {"promotion_name", promotionName},
+                {"recipients", message.To.ToString()},
+                {"subject", message.Subject},
+                {"from", message.From.ToString()},
+                {"raw_html", message.Body}
+            };
+            if (message.Bcc.Any()) param.Add("bcc", string.Join(",", message.Bcc.Select(o => o.Address)));
+            return param;
+        }
+    }
+}
